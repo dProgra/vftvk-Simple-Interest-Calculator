d9011d81a18251bfa54c6757f9304adabdb27b14
Primary <danielo21@protonmail.com>
2021-07-26 12:05:09 -0500

    changes 1

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    principal = document.getElementById("principal").value;
+    rate = document.getElementById("rate").value;
+    years = document.getElementById("years").value;
+
+    result = principal * (1.0 + rate) ** years;
     
+    return result;
 }
-        
+
+document.getElementById("result").innerHTML = compute();
